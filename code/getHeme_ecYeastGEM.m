3af8059f5b8357ad42cb75ecc40a4745ca69c51f
Ivan Domenzain <ivand@chalmers.se>
2019-10-23 17:47:34 +0200

    fix: isolate heme strain construction process

--- code/getHeme_ecYeastGEM.m
+++ code/getHeme_ecYeastGEM.m
@@ -1,6 +1,6 @@
-%function getHeme_ecYeastGEM(ecModel_batch)
-%Block alternative phosphofruktokinase:
+function newModel = getHeme_ecYeastGEM
 load('../models/ecYeastGEM.mat');
+%Block alternative phosphofruktokinase:
 ecModel_batch.ub(strcmp(ecModel_batch.rxns,'r_0887')) = 0; %ATP + sedohept-7P -> ADP + H+ + sedohept-1,7biP
 %Add heme reaction:
 posH                   = strcmp(ecModel_batch.mets,'s_3714'); %heme a [cytoplasm]
@@ -14,17 +14,4 @@ rxnsToAdd.c            = 0;
 rxnsToAdd.subSystems   = {''};
 rxnsToAdd.grRules      = {''};
 newModel               = addRxns(ecModel_batch,rxnsToAdd,1,'c',false);
-rxnTarget               = 'r_s3714_Ex';
-cSource   = 'D-glucose exchange (reversible)';
-alphaLims = [0.2 0.8];
-Nsteps    = 16;
-git ('clone https://github.com/SysBioChalmers/GECKO')
-cd GECKO
-git checkout feat/add_FSEOF_utilities
-cd geckomat/utilities/ecFSEOF
-newFolder = '../../../../../results';
-mkdir(newFolder)
-file1 = [newFolder '/genesResults.txt'];
-file2 = [newFolder '/rxnsResults.txt'];
-results = run_ecFSEOF(newModel,rxnTarget,cSource,alphaLims,Nsteps,file1,file2);
-%end
+end
